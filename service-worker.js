c59189f4faae7ea5bacb94e5fab2abae4012f0e0
Helmut Januschka <helmut@januschka.com>
2026-08-20 23:51:11 +0200

    Add installable icon fallback

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,27 @@
-const CACHE = 'pwa-mac-mask-v1';
+const CACHE = 'pwa-mac-mask-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
